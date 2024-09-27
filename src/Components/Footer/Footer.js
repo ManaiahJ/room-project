abe164df386cb085efb754aa3910cfdd8d628e3c
ManaiahJ <manaiah.j19988@gmail.com>
2024-09-27 09:28:06 +0530

    added latest color

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -1,7 +1,7 @@
 import classes from './Footer.module.css';
 export default function Footer() {
     return (
-        <div className={classes.footer}>
+        <div className={classes.color}>
             <h1>Footer</h1>
         </div>
     )
--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -2,7 +2,7 @@
 import classes from './Header.module.css'
 export default function Footer() {
     return (
-        <div className={classes.header}>
+        <div className={classes.color}>
             <h1>Footer</h1>
         </div>
     )
